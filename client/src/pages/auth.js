2a2d168477f4ef910256cc0b24cc25afc5e00ee9
Mikyle Reyes <reyesmikyle17@gmail.com>
2024-07-23 23:30:12 -0700

    Add logic for registration

--- client/src/pages/auth.js
+++ client/src/pages/auth.js
@@ -1,9 +1,10 @@
 import { useState } from 'react'
+import axios from 'axios'
 
-const Form = ({ username, setUsername, password, setPassword, label }) => {
+const Form = ({ username, setUsername, password, setPassword, label, onSubmit }) => {
   return (
     <div className='auth-container'>
-      <form action=''>
+      <form onSubmit={onSubmit}>
         <h2>{label}</h2>
         <div className='form-group'>
           <label htmlFor='username'>Username:</label>
@@ -15,7 +16,7 @@ const Form = ({ username, setUsername, password, setPassword, label }) => {
           />
           <label htmlFor='password'>Password:</label>
           <input
-            type='text'
+            type='password'
             id='password'
             value={password}
             onChange={e => setPassword(e.target.value)}
@@ -47,6 +48,21 @@ const Register = () => {
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
 
+  const onSubmit = async e => {
+    e.preventDefault()
+    try {
+      await axios.post('http://localhost:3001/auth/register', {
+        username,
+        password,
+      })
+
+      // TODO: Make this into a modal in the redesign
+      alert('Registration Completed! You can now login.')
+    } catch (err) {
+      console.error(err)
+    }
+  }
+
   return (
     <Form
       username={username}
@@ -54,6 +70,7 @@ const Register = () => {
       password={password}
       setPassword={setPassword}
       label='Register'
+      onSubmit={onSubmit}
     />
   )
 }
