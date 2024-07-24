4e5de1fd9bfac5c41606a92cc52428ae0a041c96
Mikyle Reyes <reyesmikyle17@gmail.com>
2024-07-23 21:10:24 -0700

    Add routing system functionality

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,22 @@
 import './App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+
+import Home from './pages/home'
+import Auth from './pages/auth'
+import CreateRecipe from './pages/create-recipe'
+import SavedRecipes from './pages/saved-recipes'
 
 function App() {
   return (
     <div className='App'>
-      <h1 className='text-3xl font-bold underline'>Hello world!</h1>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/auth' element={<Auth />} />
+          <Route path='/create-recipe' element={<CreateRecipe />} />
+          <Route path='/saved-recipes' element={<SavedRecipes />} />
+        </Routes>
+      </Router>
     </div>
   )
 }
