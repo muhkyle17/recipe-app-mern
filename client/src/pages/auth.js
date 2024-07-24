93ca9112b7c7eba59442a3229b27c58f3672d6ff
Mikyle Reyes <reyesmikyle17@gmail.com>
2024-07-23 22:38:01 -0700

    Add components for login and register

--- client/src/pages/auth.js
+++ client/src/pages/auth.js
@@ -1,5 +1,46 @@
+const Login = () => {
+  return (
+    <div>
+      <p>Auth</p>
+    </div>
+  )
+}
+
+const Register = () => {
+  return (
+    <div className='auth-container'>
+      <form action=''>
+        <h2>Register</h2>
+        <div className='form-group'>
+          <label htmlFor='username'>Username:</label>
+          <input
+            type='text'
+            id='username'
+            onChange={event => {
+              console.log('event')
+            }}
+          />
+          <label htmlFor='password'>Password:</label>
+          <input
+            type='text'
+            id='password'
+            onChange={event => {
+              console.log('event')
+            }}
+          />
+        </div>
+      </form>
+    </div>
+  )
+}
+
 const Auth = () => {
-  return <div>Auth</div>
+  return (
+    <div className='auth'>
+      <Login />
+      <Register />
+    </div>
+  )
 }
 
 export default Auth
