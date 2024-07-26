7c77b0740f7495f8dab843071543ab76c2d4ca05
Mikyle Reyes <reyesmikyle17@gmail.com>
2024-07-26 12:26:40 -0700

    Display the whole list of recipes

--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -1,5 +1,46 @@
+import { useState, useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
+import axios from 'axios'
+
 const Home = () => {
-  return <div>Home</div>
+  const [recipes, setRecipes] = useState([])
+
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    const fetchRecipe = async () => {
+      try {
+        const response = await axios.get('http://localhost:3001/recipes')
+        setRecipes(response.data)
+      } catch (err) {
+        console.error(err)
+      }
+    }
+
+    fetchRecipe()
+  }, [])
+
+  return (
+    <div>
+      <h1>Recipes</h1>
+      <ul>
+        {recipes.map(recipe => {
+          return (
+            <li key={recipe._id}>
+              <div>
+                <h2>{recipe.name}</h2>
+              </div>
+              <div className='instructions'>
+                <p>{recipe.instructions}</p>
+              </div>
+              <img src={recipe.imageUrl} alt={recipe.name} />
+              <p>Cooking Time: {recipe.cookingTime} (minutes)</p>
+            </li>
+          )
+        })}
+      </ul>
+    </div>
+  )
 }
 
 export default Home
