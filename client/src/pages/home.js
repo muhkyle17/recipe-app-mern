82091f29221054e902810dfa97aae40b06cfb018
Mikyle Reyes <reyesmikyle17@gmail.com>
2024-07-26 13:21:15 -0700

    Add functionality for saving recipes

--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -1,9 +1,11 @@
 import { useState, useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
 import axios from 'axios'
+import { useGetUserID } from '../hooks/useGetUserID'
 
 const Home = () => {
   const [recipes, setRecipes] = useState([])
+  const userID = useGetUserID()
 
   const navigate = useNavigate()
 
@@ -20,6 +22,19 @@ const Home = () => {
     fetchRecipe()
   }, [])
 
+  const saveRecipe = async recipeID => {
+    console.log(recipeID, 'recipeID')
+    try {
+      const response = await axios.put('http://localhost:3001/recipes', {
+        recipeID,
+        userID,
+      })
+      console.log(response)
+    } catch (err) {
+      console.error(err)
+    }
+  }
+
   return (
     <div>
       <h1>Recipes</h1>
@@ -29,6 +44,7 @@ const Home = () => {
             <li key={recipe._id}>
               <div>
                 <h2>{recipe.name}</h2>
+                <button onClick={() => saveRecipe(recipe._id)}>Save</button>
               </div>
               <div className='instructions'>
                 <p>{recipe.instructions}</p>
