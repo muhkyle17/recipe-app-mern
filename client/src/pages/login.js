ae46eb7922ed840879117b6cd522d5470a8e1b3d
Mikyle Reyes <reyesmikyle17@gmail.com>
2024-07-30 15:59:31 -0700

    Init login and register pages

---
+++ client/src/pages/login.js
@@ -0,0 +1,44 @@
+import { useState } from 'react'
+import axios from 'axios'
+import { useCookies } from 'react-cookie'
+import { useNavigate } from 'react-router-dom'
+
+import Form from './auth'
+
+const Login = () => {
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+
+  const [_, setCookies] = useCookies(['access_token'])
+  const navigate = useNavigate()
+
+  const onSubmit = async e => {
+    e.preventDefault()
+
+    try {
+      const response = await axios.post('http://localhost:3001/auth/login', {
+        username,
+        password,
+      })
+
+      setCookies('access_token', response.data.token)
+      window.localStorage.setItem('userID', response.data.userID)
+      navigate('/')
+    } catch (err) {
+      console.error(err)
+    }
+  }
+
+  return (
+    <Form
+      username={username}
+      setUsername={setUsername}
+      password={password}
+      setPassword={setPassword}
+      label='Login'
+      onSubmit={onSubmit}
+    />
+  )
+}
+
+export default Login
---
+++ client/src/pages/register.js
@@ -0,0 +1,36 @@
+import { useState } from 'react'
+import axios from 'axios'
+
+import Form from './auth'
+const Register = () => {
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+
+  const onSubmit = async e => {
+    e.preventDefault()
+    try {
+      await axios.post('http://localhost:3001/auth/register', {
+        username,
+        password,
+      })
+
+      // TODO: Make this into a modal in the redesign
+      alert('Registration Completed! You can now login.')
+    } catch (err) {
+      console.error(err)
+    }
+  }
+
+  return (
+    <Form
+      username={username}
+      setUsername={setUsername}
+      password={password}
+      setPassword={setPassword}
+      label='Register'
+      onSubmit={onSubmit}
+    />
+  )
+}
+
+export default Register
